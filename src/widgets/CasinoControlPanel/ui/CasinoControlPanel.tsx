4a56fda9cea8f61c1c1fb24b0c79ec82d2b8beb9
Eduard <chad6804@gmail.com>
2026-02-13 14:03:39 +0300

    Сокращение кулдауна спина

--- src/widgets/CasinoControlPanel/ui/CasinoControlPanel.tsx
+++ src/widgets/CasinoControlPanel/ui/CasinoControlPanel.tsx
@@ -60,7 +60,7 @@ export const CasinoControlPanel: React.FC<CasinoControlPanelProps> = ({
     useEffect(() => {
         if (!isSpinning && !isResolving) {
             setIsCooldown(true);
-            const cooldownTime =  isTurbo ? 2000 : 4000;
+            const cooldownTime =  isTurbo ? 1000 : 2000;
             const timer = setTimeout(() => setIsCooldown(false), cooldownTime);
             return () => clearTimeout(timer);
         }
