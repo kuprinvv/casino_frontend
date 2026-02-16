820b46baa8530b50c5b0e1cb341b74737933b4eb
Eduard <chad6804@gmail.com>
2026-02-16 13:34:17 +0300

    Доработка адаптации экрана 3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,10 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import {NeonLoading} from "@widgets/Loading/NeonLoading.tsx";
 
 const GamesMenuPage = React.lazy(() => import('@pages/GamesMenuPage'));
 const GamePage = React.lazy(() => import('@pages/GamePage'));
 const CascadeGamePage = React.lazy(() => import('@pages/CascadeGamePage'));
-
-const preloadGameAssets = () => {
-    const assets = [
-        '/1.png', '/2.png', '/3.png', '/4.png', '/5.png', '/6.png', '/7.png',
-        '/8.png', '/9.png', '/auto.png', '/bg.png', '/play.png', '/turbo.png',
-        '/turbo2.png', '/w.png', '/wild.png', '/board.png', '/bonus.png',
-        '/bonus-btn.png', '/bonus-btn2.png', '/5.mp4', '/bonusVideo.mp4'
-    ];
-
-    assets.forEach(src => {
-        if (src.endsWith('.mp4')) {
-            const video = document.createElement('video');
-            video.src = src;
-            video.preload = 'auto';
-        } else {
-            const img = new Image();
-            img.src = src;
-        }
-    });
-};
 
 
 const router = createBrowserRouter([
@@ -55,9 +35,6 @@ const router = createBrowserRouter([
 ]);
 
 export const App: React.FC = () => {
-    useEffect(() => {
-        preloadGameAssets();
-    }, []);
 
     return (
         <div className="app-container">
